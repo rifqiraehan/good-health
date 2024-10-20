bf167a0b78ca1c1e1ecfaa5303c19f2a7e395740
rifqiraehan <rifqiraehan86@gmail.com>
2024-10-21 06:15:08 +0700

    first commit

--- lib/util/config/config_mobile.dart
+++ lib/util/config/config_mobile.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AppConfig {
   static String get API_HOST {
     if (Platform.isAndroid || Platform.isIOS) {
-      return "192.168.254.102"; // Change this to your local IPv4 address
+      return "192.0.0.0.1"; // Change this to your local IPv4 address
     } else {
       return "localhost";
     }
