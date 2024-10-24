fc9e636c63c99b4a4f43619d89a8cc79ced16b2f
rifqiraehan <rifqiraehan86@gmail.com>
2024-10-25 06:11:10 +0700

    Refactor import statement for main.dart in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:good_health/archives/main.dart';
+import 'package:good_health/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
